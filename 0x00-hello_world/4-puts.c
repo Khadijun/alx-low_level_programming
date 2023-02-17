bc5c9f14c0a7525a556c725e8fc966725d2e2ab7
Khadijun <muhachej@gmail.com>
2023-02-17 02:17:49 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <Stdio.h>
+1 #include <stdio.h>
+2 /**
+3   * Main-Prints out a string to stdout.
+4   * Return: 0 if success.
+5   * /
+6 int main(void)
+7	{	
+8	puts("Programming is like building a multilingual puzzle");
+9	return (0);
+10      }
 
-/**
- *Description: main prints out a string to stdout
- *Return: 0 if successful
- */
-int main(void)
-{
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
-}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+1 #include <stdio.h>
+2
+3 /*
+4  * Main-prints out a string
+5  * Return : 0 if successful
+6  * /
+7 int main(void);
+8 {
+9	printf("with proper grammar, but the outcome is a piece of art");
+10	returns (0);
+11}
+
