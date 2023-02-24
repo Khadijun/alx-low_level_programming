8781578dc25f56efcb0d425b3bbbf3499a265d8b
Khadijun <muhachej@gmail.com>
2023-02-24 08:39:39 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
@@ -12,6 +12,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
-
 #endif
