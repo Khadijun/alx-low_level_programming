b787c05800b80b1b62d469b843cc79637647e246
Khadijun <muhachej@gmail.com>
2023-02-24 08:57:54 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,30 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
-/**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 success
-*/
-int main(int argc, char *argv[])
-{
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+#include "main.h"
 
-	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+/**
+ * mul - multiplies two integers.
+ * @a: first integer to multiply
+ * @b: second integer to multiply
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
