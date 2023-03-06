44fb02a815130645336b0a95bb6c66a103a588b5
Khadijun <muhachej@gmail.com>
2023-03-06 15:10:00 -0800

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *_memcpy- a function that copies memory area
+  *@dest: memory where the copied  files are stored
+  *@src: memory where the files are copied from
+  *@n: number of bytes
+  * Return: copied memory with n bytes changed
+  */
+
+char *_strchr(char *s, char c)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
