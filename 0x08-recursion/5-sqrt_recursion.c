eb7496d74f449da430ba7ae199e9cfcb277b473b
Khadijun <muhachej@gmail.com>
2023-03-08 06:51:43 -0800

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (_sqrt_recursion(n, 0));
 }
 /**
   *actual_sqrt_recursion-recurses to find the natural square root
@@ -22,5 +22,5 @@ int actual_sqrt_recursion(int n, int i)
 	return (-1);
 	if (i * i == n)
 	return (i);
-	return (actual_sqrt_recurion(n, i + 1));
+	return (actual_sqrt_recursion(n, i + 1));
 }
