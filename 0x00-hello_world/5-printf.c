9d48601238b1012b40fcbe3c403e88b1d29e693f
“khadijun” <muhachej@gmail.com>
2023-12-14 03:41:47 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main- entry point
+  * printf- used to print formatted output to the standard output stdout
+  * Return: is 0
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
