c488280463a7067099745afbf668f340d20fe889
“khadijun” <muhachej@gmail.com>
2023-12-14 03:25:18 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * puts - used to write a line or string to the output( stdout ) stream
+  * Return: 0 on success
+  */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
