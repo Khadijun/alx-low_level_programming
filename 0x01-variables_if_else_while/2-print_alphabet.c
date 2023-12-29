1f2e500d43e473636e1582b74a727c16309352f8
“khadijun” <muhachej@gmail.com>
2023-12-29 02:26:03 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main-prints the alphabet in lowercase
+  * Return: returns 0
+  */
+
+int main(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
