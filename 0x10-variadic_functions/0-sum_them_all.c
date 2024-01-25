9500169c17362679dc33330357552fbdc37815fa
Khadijun <muhachej@gmail.com>
2024-01-25 13:33:22 -0800

    Tasks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,6 +5,7 @@
   * sum_them_all - a function that returns the sum of all its parameters.
   * @n: the number of parameters in the function
   * Return: returns 0
+  * @...: parameters
   */
 
 int sum_them_all(const unsigned int n, ...)
