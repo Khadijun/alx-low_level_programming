9d0f61bc38dca62b701d6c0e944067aa662f960e
Khadijun <muhachej@gmail.com>
2024-03-25 12:25:00 +0300

    Task

---
+++ 0x18-dynamic_libraries/multiply.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * mul- two integers
+  * @a: first integer
+  * @b: second integer
+  * Return: 0
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
