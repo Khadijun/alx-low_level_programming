5637a1dbfdce46f73aba78809e6fcbb9c85155b9
Khadijun <muhachej@gmail.com>
2024-06-24 01:36:44 -0700

    Task 1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
 # Source files
-SRC_FILES="0-isupper.c  0-strcat.c  1-isdigit.c  1-strncat.c  2-strlen.c   3-islower.c  3-strcmp.c  4-isalpha.c  5-strstr.c  9-strcpy.c  _putchar.c 0-memset.c   100-atoi.c  1-memcpy.c   2-strchr.c   2-strncpy.c  3-puts.c   3-strspn.c  4-strpbrk.c  6-abs.c main.c"
+SRC_FILES="0-isupper.c 0-memset.c 0-strcat.c 100-atoi.c 1-isdigit.c 1-memcpy.c 1-strncat.c 2-strchr.c 2-strlen.c 2-strncpy.c 3-islower.c 3-puts.c 3-strcmp.c 3-strspn.c 4-isalpha.c 4-strpbrk.c 5-strstr.c 6-abs.c 9-strcpy.c main.c _putchar.c source_0.c source_1.c source_2.c"
 
-# Compiler options
-CC=gcc
-#CFLAGS="-Wall -Werror -Wextra -pedantic -std=gnu89"
-
-# Output static library name
-OUTPUT_LIB="liball.a"
-
-# Compile each source file into object files
-for file in $SRC_FILES; do
-    $CC $CFLAGS -c $file -o ${file%.c}.o
-    done
-
-# Create the static library
-    ar rcs $OUTPUT_LIB *.o
-
-# Clean up object files
-    rm -f *.o
-
-    echo "Static library $OUTPUT_LIB  created successfully."
-
+# Compile  
+ gcc -c $SRC_FILES
+ 
+ 
+#Create the static library
+ar rcs liball.a  *.o
