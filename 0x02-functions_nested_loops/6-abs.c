e6315e37bc4fab21c031c835f51c0569a8e72bfb
Khadijun <muhachej@gmail.com>
2025-04-06 16:59:32 +0300

    abs function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_abs - displays absolute value of an integer
+  *@x: parameter that specifies a number
+  *Return: returns positive integers
+  */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+	else
+	{
+		return (x);
+	}
+}
